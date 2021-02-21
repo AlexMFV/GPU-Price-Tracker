2822f0c2fe62caf7667d6bd34d46d4d72ba8edc5
AlexMFV <alexmfv1@gmail.com>
2021-02-21 15:35:49 +0000

    Simple List Test

---
+++ NETCore/GPU.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETCore
+{
+    public class GPU
+    {
+        public string url { get; set; }
+        public string gpuName { get; set; }
+        public string price{ get; set; }
+        public string availability { get; set; }
+        public string country { get; set; }
+
+        public GPU(string _url, string _gpuName, string _price, string _avail, string _country = null)
+        {
+            this.url = _url;
+            this.gpuName = _gpuName;
+            this.price = _price;
+            this.availability = _avail;
+            if(_country != null) this.country = _country;
+        }
+    }
+}
---
+++ NETCore/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace NETCore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BindingList<GPU> gpuCollection = new BindingList<GPU>();        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Timer tim = new Timer(1000);
+            tim.Elapsed += T_Elapsed;
+
+            tim.Start();
+            lstListings.DataContext = gpuCollection;
+            gpuCollection.AllowEdit = true;
+            gpuCollection.AddingNew += GpuCollection_AddingNew;
+            gpuCollection.ListChanged += GpuCollection_ListChanged;
+        }
+
+        private void GpuCollection_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            lstListings.DataContext = null;
+            lstListings.DataContext = gpuCollection;
+        }
+
+        private void GpuCollection_AddingNew(object sender, AddingNewEventArgs e)
+        {
+            lstListings.DataContext = null;
+            lstListings.DataContext = gpuCollection;
+        }
+
+        private void T_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            changeValues();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            gpuCollection.Add(new GPU("www.google.pt", "GTX 1080Ti", "349€", "Out of Stock"));
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            if(gpuCollection.Count > 0)
+                gpuCollection.Remove(gpuCollection.Last());
+        }
+
+        void changeValues()
+        {
+            Random r;
+            for (int i = 0; i < gpuCollection.Count; i++)
+            {
+                r = new Random(DateTime.Now.Millisecond);
+                int value = r.Next();
+                gpuCollection[i].price = Convert.ToString(value) + "€";
+            }
+        }
+    }
+}
